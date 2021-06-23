74c4cb7cb3a048f2070d8d62e2a177dd29484162
mihail <tarakanov.m.a@mail.ru>
2021-06-23 20:41:28 +0300

    Ensuring preconditions

--- fixture/contact.py
+++ fixture/contact.py
@@ -53,3 +53,8 @@ class ContactHelper:
         # submit modification
         wd.find_element_by_name("update").click()
         self.return_to_home_page()
+
+    def count(self):
+        wd = self.app.wd
+        self.app.open_home_page()
+        return len(wd.find_elements_by_name("selected[]"))
--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -3,7 +3,11 @@ from model.contact import Contact
 
 
 def test_modify_contact_firstname(app):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(firstname='test'))
     app.contact.modify_first_contact(Contact(firstname="New firstname"))
 
 def test_modify_contact_lastname(app):
-    app.contact.modify_first_contact(Contact(firstname="New lastname"))
+    if app.contact.count() == 0:
+        app.contact.create(Contact(firstname='test'))
+    app.contact.modify_first_contact(Contact(lastname="New lastname"))

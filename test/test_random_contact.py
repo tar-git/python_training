23bafb7bf548ed20eef7e34a01be0e6c401cc0bd
mihail <tarakanov.m.a@mail.ru>
2021-07-01 23:02:11 +0300

    fixed random contact test

--- test/test_random_contact.py
+++ test/test_random_contact.py
@@ -31,11 +31,9 @@ def merge_phones_like_on_home_page(contact):
 
 
 def merge_emails_like_on_home_page(contact):
-    return "\n".join(filter(lambda x: x!="",
-                        map(lambda x: clear(x),
-                            filter(lambda x: x is not None,
-                                [
-                                    contact.email,
-                                    contact.email2,
-                                    contact.email3
-                                ]))))
+    return "\n".join(filter(lambda x: x is not None and x!="",
+    [
+        contact.email,
+        contact.email2,
+        contact.email3
+    ]))

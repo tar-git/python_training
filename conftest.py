14935594ff550f7c4bfcc29fe71013d319ab6f27
mihail <tarakanov.m.a@mail.ru>
2021-07-02 18:10:29 +0300

    Added cli options

--- conftest.py
+++ conftest.py
@@ -4,15 +4,18 @@ from fixture.application import Application
 fixture = None
 
 @pytest.fixture
-def app():
+def app(request):
     global fixture
+    browser = request.config.getoption("--browser")
+    base_url = request.config.getoption("--baseUrl")
     if fixture is None:
-        fixture = Application()
+        fixture = Application(browser=browser, base_url=base_url)
     else:
         if not fixture.is_valid():
-            fixture = Application()
+            fixture = Application(browser=browser, base_url=base_url)
     fixture.session.ensure_login(username="admin", password="secret")
     return fixture
+
 
 @pytest.fixture(scope="session", autouse=True)
 def stop(request):
@@ -21,3 +24,8 @@ def stop(request):
         fixture.destroy()
     request.addfinalizer(fin)
     return fixture
+
+
+def pytest_addoption(parser):
+    parser.addoption("--browser", action="store", default="firefox")
+    parser.addoption("--baseUrl", action="store", default="http://localhost/addressbook/")
--- fixture/application.py
+++ fixture/application.py
@@ -8,12 +8,20 @@ from fixture.common import wait_for
 
 class Application:
 
-    def __init__(self):
-        self.wd = webdriver.Firefox()
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "ie":
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError("Unrecognized %s" % browser)
         # self.wd.implicitly_wait(1)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
+        self.base_url = base_url
 
     def is_valid(self):
         try:
@@ -26,7 +34,7 @@ class Application:
         wd = self.wd
         if not (wd.current_url == ("http://localhost/addressbook/index.php")
            and len(wd.find_elements_by_link_text("Last name")) > 0):
-            wd.get("http://localhost/addressbook/")
+            wd.get(self.base_url)
 
     def destroy(self):
         self.wd.quit()
